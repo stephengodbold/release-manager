05016edaf9e5e4cb70a788e81cc35fd87b362fe7
Stephen Godbold <stephen.godbold@gmail.com>
2012-11-22 12:47:13 +1100

    Tiny bit of code cleanup

--- ReleaseManager.Tests/Environment_Service.cs
+++ ReleaseManager.Tests/Environment_Service.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
-using ReleaseManager.Models;
 using ReleaseManager.Queries;
 using ReleaseManager.Services;
 using Shouldly;
