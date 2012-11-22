85f81360d985a84d0d9387ce2eb60e3a6db1c70b
Stephen Godbold <stephen.godbold@gmail.com>
2012-11-22 14:19:00 +1100

    Added linking to release notes

---
+++ ReleaseManager/Controllers/ReleaseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReleaseManager.Controllers
+{
+    public class ReleaseController : Controller
+    {
+        public ActionResult Index(string currentRelease, string previousRelease)
+        {
+            return View();
+        }
+
+    }
+}
