b80590bf56b93d14df807da21ce79795e30515c6
Steve Godbold <stephen.godbold@gmail.com>
2013-09-08 16:19:37 +1000

    Added test for production header response

--- ReleaseManager.API.Tests/Demo Mode/When_Asked_For_Demo_Environments.cs
+++ ReleaseManager.API.Tests/Demo Mode/When_Asked_For_Demo_Environments.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Net;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReleaseManager.API.App_Start;
 using Shouldly;
 
 namespace ReleaseManager.API.Tests.Demo_Mode
@@ -10,6 +11,17 @@ namespace ReleaseManager.API.Tests.Demo_Mode
     // ReSharper disable InconsistentNaming
     public class When_Set_To_Mode : WebApiTestBase
     {
+        [TestMethod, TestCategory("Integration")]
+        public void Should_Respond_With_Production_Header()
+        {
+            var request = WebRequest.Create(BaseUri + "/api/environments");
+            request.Headers.Add("x-api-mode", ApiMode.Production.ToString());
+            var responseHeaders = request.GetResponse().Headers;
+
+            responseHeaders.AllKeys.ShouldContain("x-api-mode");
+            responseHeaders.Get("x-api-mode").ShouldBe(ApiMode.Production.ToString(), Case.Insensitive);
+        }
+        
         [TestMethod, TestCategory("Integration")]
         public void Should_Respond_With_Demo_Header()
         {
