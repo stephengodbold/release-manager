d1ada3955f87a5c76534cb7c2da30e9c4b05f26b
Steve Godbold <stephen.godbold@readify.net>
2013-10-16 10:23:00 +1100

    Resolving forgotten merge conflicts

--- ReleaseManager.API/Controllers/NotesController.cs
+++ ReleaseManager.API/Controllers/NotesController.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Web.Http;
 using Autofac.Features.Indexed;
 using ReleaseManager.API.App_Start;
@@ -33,39 +32,3 @@ namespace ReleaseManager.API.Controllers
 
     }
 }
-=======
-﻿using System.Web.Http;
-using Autofac.Features.Indexed;
-using ReleaseManager.API.App_Start;
-using ReleaseManager.API.Common;
-using ReleaseManager.API.Models;
-using ReleaseManager.API.Services;
-
-namespace ReleaseManager.API.Controllers
-{
-    public class NotesController : ApiController
-    {
-
-        private readonly IIndex<ApiMode, IWorkItemService> workItemServiceByMode;
-        private IWorkItemService workItemService;
-
-        public NotesController(IIndex<ApiMode, IWorkItemService> workItemServiceByMode)
-        {
-            this.workItemServiceByMode = workItemServiceByMode;
-        }
-
-        public ReleaseNotes Get(string earlierBuild, string laterBuild)
-        {
-            ResolveWorkItemService();
-            return workItemService.GetReleaseNotes(earlierBuild, laterBuild);
-        }
-
-        private void ResolveWorkItemService()
-        {
-            var apiMode = ControllerContext.Request.GetApiMode();
-            workItemService = workItemServiceByMode[apiMode];
-        }
-
-    }
-}
->>>>>>> c95527e5bdcad2ac94de132c1ef1acdfce80fa27
